fa818f79da306c0d5e967f2daab4904fbff4fae0
Robin Hopkins <robinjohnhopkins@hotmail.com>
2020-01-06 10:36:07 +0000

    awesome animated canvas background that lets clicks through - uses z-order

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo building...
-mvn install && java -jar  target/angular-maven-0.0.1-SNAPSHOT.jar
+mvn install -DskipTests  && java -jar  target/angular-maven-0.0.1-SNAPSHOT.jar
 
 echo yeh baby
 
